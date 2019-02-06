d8d4404145d12dffe422136dbda2a6e95d9cdbda
Nabeel Khowaja <nabeel.khowaja@ihsinformatics.com>
2019-02-06 16:21:10 +0500

    LiveData working.

--- app/src/main/java/com/example/jetpackfun/repositories/LocationRepository.kt
+++ app/src/main/java/com/example/jetpackfun/repositories/LocationRepository.kt
@@ -1,12 +1,7 @@
 package com.example.jetpackfun.repositories
 
-import com.example.jetpackfun.models.Location
 import com.example.jetpackfun.network.APIService
 import com.example.jetpackfun.network.OnResponseReceivedListener
-import com.example.jetpackfun.network.RestClient
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
 
 object LocationRepository {
 
--- app/src/main/java/com/example/jetpackfun/viewmodels/MainActivityViewModel.kt
+++ app/src/main/java/com/example/jetpackfun/viewmodels/MainActivityViewModel.kt
@@ -1,5 +1,6 @@
 package com.example.jetpackfun.viewmodels
 
+import android.util.Log.d
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.jetpackfun.models.Location
@@ -7,6 +8,8 @@ import com.example.jetpackfun.network.OnResponseReceivedListener
 import com.example.jetpackfun.repositories.LocationRepository
 
 class MainActivityViewModel : ViewModel() {
+
+    val TAG: String = "MainActivityViewModel"
 
     var mLocations: MutableLiveData<List<Location>> = MutableLiveData()
     val mIsUpdating: MutableLiveData<Boolean> = MutableLiveData()
@@ -26,7 +29,10 @@ class MainActivityViewModel : ViewModel() {
                 mIsUpdating.postValue(false)
             }
 
-            override fun onFailureReceived(errorMessage: String?) {}
+            override fun onFailureReceived(errorMessage: String?) {
+                d(TAG, errorMessage)
+                mIsUpdating.postValue(false)
+            }
         })
     }
 }
